fb4b0899f9e196ab37f57cbc767592258191d29d
Navid Dezashibi <dezashibi@gmail.com>
2024-03-08 11:57:37 +0400

    sprite map generator

--- src/helpers/sprite_generator.ts
+++ src/helpers/sprite_generator.ts
@@ -1,6 +1,6 @@
-import { Scene, Sprite, SpriteManager, Vector3 } from "babylonjs";
+import { AssetsManager, Engine, Scene, Sprite, SpriteManager, SpriteMap, SpritePackedManager, Texture, Vector2, Vector3 } from "babylonjs";
 
-export async function spriteManagerGenerator(
+export async function spriteRandomGenerator(
     scene: Scene,
     name: string,
     spriteUrl: string,
@@ -38,4 +38,63 @@ export async function spriteManagerGenerator(
             sprite.playAnimation(0, numberOfFrames!, true, 150);
         }
     }
+}
+
+export async function spritePackRandomGenerator(
+    spritePack: SpritePackedManager,
+    name: string,
+    spriteCellRef: string,
+    qty: number,
+    min: number,
+    max: number,
+    zPos: number,
+    spriteWidth: number,
+    spriteHeight: number
+): Promise<void> {
+    for (let i = 1; i < qty; ++i) {
+        const randomPosX = Math.random() * (max - min) + min;
+        const randomPosY = Math.random() * (max - min) + min;
+
+        const sprite = new Sprite(`${name}-${i}`, spritePack);
+
+        sprite.cellRef = spriteCellRef;
+
+        sprite.width = spriteWidth;
+        sprite.height = spriteHeight;
+        sprite.position = new Vector3(randomPosX, randomPosY, zPos);
+    }
+}
+
+export async function spriteMapGenerator(
+    scene: Scene,
+    textureUrl: string,
+    titleMapJsonUrl: string,
+    titleMapFileUrl: string,
+    bgSize: Vector2,
+    outputSize: Vector2
+) {
+    const spriteSheet = new Texture(textureUrl, scene, false, false, Texture.NEAREST_NEAREST, null, null, null, false, Engine.TEXTUREFORMAT_RGBA);
+
+    const assetsManager = new AssetsManager(scene);
+
+    assetsManager.autoHideLoadingUI = true;
+
+    const textTask = assetsManager.addTextFileTask("taskBackground", titleMapJsonUrl);
+
+    textTask.onSuccess = task => {
+        let atlasJson = JSON.parse(task.text);
+
+        let spriteMap = new SpriteMap('sprite-map', atlasJson, spriteSheet, {
+            stageSize: bgSize,
+            outputSize,
+            outputPosition: Vector3.Zero(),
+            outputRotation: Vector3.Zero(),
+            baseTile: 0,
+            flipU: true
+        }, scene);
+
+        spriteMap.loadTileMaps(titleMapFileUrl);
+    }
+
+    assetsManager.loadAsync();
 }
--- src/states/main_menu.ts
+++ src/states/main_menu.ts
@@ -1,12 +1,12 @@
-import { ArcRotateCamera, Scene, Vector3 } from "babylonjs";
+import { DirectionalLight, FreeCamera, Scene, SpritePackedManager, Vector2, Vector3 } from "babylonjs";
 import { Game } from "../Game";
 import { State } from "../defs";
 import { AdvancedDynamicTexture, Control, Image } from "babylonjs-gui";
 import { simpleButton } from "../helpers/gui_generator";
-import { spriteManagerGenerator } from "../helpers/sprite_generator";
+import { spriteMapGenerator, spritePackRandomGenerator, spriteRandomGenerator } from "../helpers/sprite_generator";
 
 const createCamera = function (scene: Scene) {
-    const camera = new ArcRotateCamera('camera', Math.PI, Math.PI, 1, Vector3.Zero(), scene);
+    const camera = new FreeCamera('camera', Vector3.Zero(), scene);
 
     camera.attachControl(true);
 
@@ -16,7 +16,27 @@ const createCamera = function (scene: Scene) {
 async function createTheAnimatedBG(scene: Scene) {
     const min = -15;
     const max = 15;
-    spriteManagerGenerator(scene, "rabbit", "assets/sprites/animals/rabbit_watching.png", 600, 600, 5, min, max, -0.6, 1, 1, true, 11);
+
+    spriteRandomGenerator(scene, "rabbit", "assets/sprites/animals/rabbit_watching.png", 600, 600, 5, min, max, -0.6, 1, 1, true, 11);
+
+    const spritePackManager = new SpritePackedManager("env-sprite-manager", "assets/sprites/environment/envSpritePack.png", 100, scene);
+
+    spritePackRandomGenerator(spritePackManager, "tree-triangle", "tree_triangle.png", 20, min, max, -1.8, 2.52 / 2, 7.76 / 2);
+    spritePackRandomGenerator(spritePackManager, "tree-round", "tree_round.png", 20, min, max, -2, 5.52 / 2, 8.93 / 2);
+    spritePackRandomGenerator(spritePackManager, "tree-dead", "tree_dead.png", 5, min, max, -1.2, 3.69 / 2, 5.77 / 2);
+
+    spritePackRandomGenerator(spritePackManager, "bush1", "bush_1.png", 20, min, max, -0.1, 2.48 / 4, 2.28 / 4);
+    spritePackRandomGenerator(spritePackManager, "bush2", "bush_2.png", 20, min, max, -0.1, 2.14 / 4, 2.16 / 4);
+    spritePackRandomGenerator(spritePackManager, "flower-red", "flower_red.png", 20, min, max, -0.1, 0.79 / 4, 2.1 / 4);
+
+    await spriteMapGenerator(
+        scene,
+        "assets/textures/tile_map_texture.png",
+        "assets/tilemaps/tile_map_texture.json",
+        "assets/tilemaps/menu_tile_map.tilemaps",
+        new Vector2(4, 4),
+        new Vector2(40, 40)
+    );
 }
 
 const createLogo = function (container: AdvancedDynamicTexture) {
@@ -71,6 +91,9 @@ export default async function (this: Game) {
 
     const sceneToLoad = new Scene(this.engine);
     createCamera(sceneToLoad);
+
+    const light = new DirectionalLight("light", new Vector3(0, 1, 1), sceneToLoad);
+    light.intensity = 0.4;
 
     await createTheAnimatedBG(sceneToLoad);
     await createGUI.call(this, sceneToLoad);
